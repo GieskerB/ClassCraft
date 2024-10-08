cc9d23d90776b0c60dbe9acdc14c89403e91b843
GieskerB <bjarne.giesker@gmail.com>
2024-10-08 14:05:31 +0200

    start with kotlin -> Main.kt

---
+++ src/main/kotlin/de/gieskerb/class_craft/Main.kt
@@ -0,0 +1,53 @@
+package de.gieskerb.class_craft
+
+import de.gieskerb.class_craft.commands.ClassCommand
+import de.gieskerb.class_craft.commands.HorseCommand
+import de.gieskerb.class_craft.listener.*
+import org.bukkit.Bukkit
+import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    // TODO in general: more and colored text!
+
+    companion object {
+        private lateinit var plugin: JavaPlugin
+
+        @JvmStatic
+        fun getPlugin(): JavaPlugin {
+            return plugin;
+        }
+    }
+
+    init {
+        plugin = this
+    }
+
+    override fun onEnable() {
+        // Plugin startup logic
+        getCommand("class")?.setExecutor(ClassCommand())
+        getCommand("horse")?.setExecutor(HorseCommand())
+
+        val pluginManager: PluginManager = Bukkit.getPluginManager()
+        pluginManager.registerEvents(BlockBreakListener(), this)
+        pluginManager.registerEvents(BlockPlaceListener(), this)
+        pluginManager.registerEvents(EntityBreedListener(), this)
+        pluginManager.registerEvents(EntityDeathListener(), this)
+        pluginManager.registerEvents(HarvestBlockListener(), this)
+        pluginManager.registerEvents(HungerChangeListener(), this)
+        pluginManager.registerEvents(InventoryClickListener(), this)
+        pluginManager.registerEvents(InventoryOpenListener(), this)
+        pluginManager.registerEvents(PlayerJoinListener(), this)
+        pluginManager.registerEvents(PlayerMoveListener(), this)
+        pluginManager.registerEvents(PlayerQuitListener(), this)
+        pluginManager.registerEvents(RespawnListener(), this)
+        pluginManager.registerEvents(WorldSaveListener(), this)
+
+        PlayerJoinListener.loadAllPlayersData()
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+        PlayerQuitListener.saveAllPlayersData()
+    }
+}
